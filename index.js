2075bedfcbb29239c1a6117d08563f2a641bfb22
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2024-09-12 14:37:01 +0800

    Refactor database connection and user schema in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1/shop_db')
+mongoose.connect('mongodb://127.0.0.1/relation_db')
 .then((result) => {
     console.log('Connected to the database');
 }).catch((err) => {
@@ -10,6 +10,7 @@ mongoose.connect('mongodb://127.0.0.1/shop_db')
 const userSchema = new mongoose.Schema({
     name: String,
     addresses: [{
+        _id: false,
         street: String,
         city: String,
         country: String
@@ -18,4 +19,29 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-const makeUser
+// const makeUser = async () => {
+//     const user = new User ({
+//         name: 'Kahfi'
+//     })
+//     user.addresses.push({
+//         street: 'Tamaona',
+//         city: 'Bulukumba',
+//         country: 'Indonesia'
+//     })
+
+//     const res = await user.save();
+//     console.log(res);    
+// }
+// makeUser();
+
+// const addAddresses = async (id) => {
+//     const user = await User.findById(id)
+//     user.addresses.push({
+//         street: 'Sam ratulangi',
+//         city: 'Makassar',
+//         country: 'INA'
+//     })
+//     const res = await user.save();
+//     console.log(res)
+// }
+// addAddresses('66e2899aa18f7da3e03368f7')
